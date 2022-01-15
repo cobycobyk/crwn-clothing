9917869b2301cfb0d9f9b7bf35ac6533034b3f4d
Coby Kabili <ckabili8@gmail.com>
2022-01-14 18:40:13 -0700

    homepage components and styling

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,40 +1,10 @@
 import './HomePage.scss';
+import Directory from '../../components/Directory/Directory';
 
 export default function HomePage() {
     return (
         <div className="homepage">
-            <div className="directory-menu">
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">VIDEOS</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">STILLS</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">AUDIO</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">BOXING</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">FOOTBALL</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-            </div>
+            <Directory />
         </div>
     )
 }
